8bfa5c873d717df3f448af21de49f10f07ede188
VladStoyanoff <vladstoyanoff@gmail.com>
2022-09-28 21:48:56 +0100

    Update Radar (1.0.0)

--- Lobo/Assets/Scripts/DestroyBase.cs
+++ Lobo/Assets/Scripts/DestroyBase.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class DestroyBase : MonoBehaviour
 {
     [SerializeField] GameObject enemyBase;
+    Spawner spawner;
+
+    void Awake()
+    {
+        spawner = FindObjectOfType<Spawner>();
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player Bullet"))
         {
             Destroy(enemyBase);
+            spawner.GetEnemyBases().Remove(enemyBase);
         }
     }
+
+
 }
--- Lobo/Assets/Scripts/Navigation.cs
+++ Lobo/Assets/Scripts/Navigation.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Navigation : MonoBehaviour
 {
     [SerializeField] GameObject radar;
     [SerializeField] List<SpriteRenderer> sprites;
-    [SerializeField] float[] arrayWithConstants;
     Spawner spawner;
 
     void Awake()
     {
         spawner = FindObjectOfType<Spawner>();
-        
     }
 
     void Update()
@@ -24,26 +23,87 @@ public class Navigation : MonoBehaviour
 
     void UpdateEnemyBaseLocations()
     {
-        for (int i = 0; i < 8; i++)
-        {
-            var checkConstant = arrayWithConstants[i];
-            var checkConstant2 = arrayWithConstants[i + 1];
-            CheckRadarDirection(checkConstant, checkConstant2);
-        }
-    }
-
-    void CheckRadarDirection(int a, int b)
-    {
+        // Find distances to all enemy Bases
         var enemyBases = spawner.GetEnemyBases();
-        var arrayWithDistances[];
-        foreach (GameObject enemyBase in enemyBases)
+        var vectorLengthList = new List<float>();
+        var radius = 3;
+        var baseNotOnRadar = false;
+        if (Input.GetKey(KeyCode.T))
         {
-            var distanceToBase = Vector2.Distance(gameObject, enemyBase);
-            arrayWithDistances[distanceToBase];
-            
+            for (int i = 0; i < enemyBases.Count; i++)
+            {
+                var vectorToEnemyBase = enemyBases[i].transform.position - gameObject.transform.position;
+                Debug.Log(vectorToEnemyBase);
+                var distanceBetweenPlayerBase = Vector2.Distance(gameObject.transform.position, enemyBases[i].transform.position);
+                Debug.Log(distanceBetweenPlayerBase);
+                vectorLengthList.Add(distanceBetweenPlayerBase);
+                //if (distanceBetweenPlayerBase > radius)
+                //{
+                //    baseNotOnRadar = true;
+                //    continue;
+                //}
+                Debug.Log((-.5f) * radius);
+                Debug.Log(vectorToEnemyBase.x > (-.5f) * radius);
+                Debug.Log(vectorToEnemyBase.x < (.5f) * radius);
+                Debug.Log(vectorToEnemyBase.y > 0);
+                Debug.Log(vectorToEnemyBase.y < radius);
+                if (vectorToEnemyBase.x > (-1 / 2) * radius &&
+                    vectorToEnemyBase.x < (1 / 2) * radius &&
+                    vectorToEnemyBase.y > 0 &&
+                    vectorToEnemyBase.y < radius)
+
+                {
+                    Debug.Log("?");
+                    sprites[1].gameObject.SetActive(true);
+                }
+
+                //    if (vectorToEnemyBase.x > -(Mathf.Sqrt(3)/2) * radius &&
+                //        vectorToEnemyBase.x < -(1/2) * radius &&
+                //        vectorToEnemyBase.y > 1/2 * radius &&
+                //        vectorToEnemyBase.y < Mathf.Sqrt(3) / 2 * radius)
+
+                //    {
+                //        Sprite[upleft].SetActive(true);
+                //    }
+
+                //    if (vectorToEnemyBase.x > 1 / 2 * radius &&
+                //        vectorToEnemyBase.x < Mathf.Sqrt(3) / 2 * radius &&
+                //        vectorToEnemyBase.y < Mathf.Sqrt(3) /2 * radius &&
+                //        vectorToEnemyBase.y > 1/2 * radius)
+
+                //    {
+                //        Sprite[upright].SetActive(true);
+                //    }
+
+                //    //
+                //    if (vectorToEnemyBase.x > 1 / 2 * radius &&
+                //        vectorToEnemyBase.x < Mathf.Sqrt(3) / 2 * radius &&
+                //        vectorToEnemyBase.y < Mathf.Sqrt(3) / 2 * radius &&
+                //        vectorToEnemyBase.y > 1 / 2 * radius)
+
+                //    {
+                //        Sprite[bothright].SetActive(true);
+                //    }
+                //}
+            }
         }
-        if (Vector2.Distance(gameObject, closestEnemyBase) <?> a && <?> b) return;
-        updatesprite;
+
+        
+        //if (baseNotOnRadar && allSpritesAreBlack)
+        //{
+        //    var closestenemybase = vectorLengthList.Min();
+        //    var index = vectorLengthList.IndexOf(closestenemybase);
+        //    var vectorToEnemyBase = gameObject.transform.position - enemyBases[index].transform.position;
+
+        //    // basically do the same logic as above but change radius to the length of the distance between the base and player
+
+        //}
+
+        //// Update sprite if the distance covers certain criteria
+        //if (Vector2.Distance(gameObject.transform.position, enemyBases[index].transform.position) <?> a && <?> b) return;
+
+        //// FIX: Switch case for the 4 sprites
+        //updatesprite;
     }
 
     void UpdatePlayerDirectionRadar()
