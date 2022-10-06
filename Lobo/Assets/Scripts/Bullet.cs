404a796ffda62384ff159292a4cf73f13f534b82
VladStoyanoff <vladstoyanoff@gmail.com>
2022-10-06 15:05:30 +0100

    vfx

--- Lobo/Assets/Scripts/Bullet.cs
+++ Lobo/Assets/Scripts/Bullet.cs
@@ -1,10 +1,15 @@
+using System.Collections;
+using System;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     [SerializeField] float explosionRadius = .07f;
+    [SerializeField] ParticleSystem explosion;
+
     ScoreManager scoreManager;
     AudioManager audioManager;
+
     int scoreForBasicUnit = 2;
     int scoreForChasePlayerUnit = 4;
     int scoreForRamPlayerUnit = 6;
@@ -18,11 +23,13 @@ public class Bullet : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collision)
     {
         var colliders = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+        explosion.Play();
         foreach (var col in colliders)
         {
             if (gameObject.CompareTag("Enemy Bullet") && col.CompareTag("Building Block")) continue;
             if (col.CompareTag("Wall")) continue;
             if (col.CompareTag("Player")) continue;
+            if (col.gameObject.CompareTag("Player Bullet") || col.gameObject.CompareTag("Enemy Bullet")) continue;
             audioManager.PlayHitBlockClip();
             Destroy(col.gameObject);
         }
@@ -50,6 +57,16 @@ public class Bullet : MonoBehaviour
                     }
             }
         }
+        StartCoroutine(DestroyBullet());
+       
+    }
+
+    IEnumerator DestroyBullet()
+    {
+        var durationOfTheExplosion = 1.5f;
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        yield return new WaitForSeconds(durationOfTheExplosion);
         Destroy(gameObject);
     }
 }
